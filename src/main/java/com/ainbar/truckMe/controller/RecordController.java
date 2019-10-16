fe9478e8e6edb0338f084b19fad8af1ff6f4de05
kadarH <kadar.hamza@gmail.com>
2019-10-16 01:46:17 +0100

    Export All Records (/records/all/export)

--- src/main/java/com/ainbar/truckMe/controller/RecordController.java
+++ src/main/java/com/ainbar/truckMe/controller/RecordController.java
@@ -3,11 +3,17 @@ package com.ainbar.truckMe.controller;
 import com.ainbar.truckMe.entities.Record;
 import com.ainbar.truckMe.service.BatchService;
 import com.ainbar.truckMe.service.RecordService;
+import com.opencsv.CSVWriter;
+import com.opencsv.bean.StatefulBeanToCsv;
+import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -35,6 +41,24 @@ public class RecordController {
     @GetMapping("/all")
     public List<Record> getRecords() {
         return batchService.getRecords();
+    }
+
+    @GetMapping("/all/export")
+    public void exportAllRecords(HttpServletResponse response) throws Exception {
+        String filename = "records_" + LocalDate.now() + ".csv";
+
+        response.setContentType("text/csv");
+        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + filename + "\"");
+
+        //create a csv writer
+        StatefulBeanToCsv<Record> writer = new StatefulBeanToCsvBuilder<Record>(response.getWriter())
+                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                .withSeparator(';')
+                .withOrderedResults(false)
+                .build();
+
+        writer.write(batchService.getRecords());
     }
 
     @GetMapping("/batch")
