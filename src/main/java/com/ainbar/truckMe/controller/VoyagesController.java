d136b5f4f4258e36982b9a3a9504b28611f06c0e
kadarH <kadar.hamza@gmail.com>
2019-10-23 19:05:50 +0100

    Fix Download Csv File

--- src/main/java/com/ainbar/truckMe/controller/VoyagesController.java
+++ src/main/java/com/ainbar/truckMe/controller/VoyagesController.java
@@ -7,16 +7,27 @@ import com.ainbar.truckMe.service.VoyageService;
 import com.opencsv.CSVWriter;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/voyages")
 @CrossOrigin(maxAge = 3600)
+@Slf4j
 public class VoyagesController {
 
     private BatchService batchService;
@@ -37,22 +48,42 @@ public class VoyagesController {
         return voyageService.getVoyagesByDevicetime(idCamion, date);
     }
 
-    @GetMapping("/{idCamion}/{date}/export")
-    public void exportVoyagesByIdDeviceAndDate(@PathVariable Long idCamion, @PathVariable String date, HttpServletResponse response) throws Exception {
-        String filename = "voyages_" + idCamion + "_" + LocalDate.now() + ".csv";
-        response.setContentType("text/csv");
-        response.setHeader(HttpHeaders.CONTENT_DISPOSITION,
-                "attachment; filename=\"" + filename + "\"");
+    @GetMapping(value = "/{idCamion}/{date}/export", produces = "text/csv")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<Resource> exportVoyagesByIdDeviceAndDate(@PathVariable Long idCamion, @PathVariable String date, HttpServletRequest request) throws Exception {
+        String filename = "voyages_" + idCamion + "_" + date + ".csv";
 
-        //create a csv writer
-        StatefulBeanToCsv<Voyage> writer = new StatefulBeanToCsvBuilder<Voyage>(response.getWriter())
-                .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
-                .withSeparator(';')
-                .withOrderedResults(false)
-                .build();
 
-        writer.write(voyageService.getVoyagesByDevicetime(idCamion, date));
+        List<Voyage> list = voyageService.getVoyagesByDevicetime(idCamion, date);
+        if (list != null && !list.isEmpty()) {
+            Writer writer = new FileWriter("/Users/mac/Documents/PROJECTS/truckMe/src/main/resources/" + filename);
+            StatefulBeanToCsv<Voyage> writers = new StatefulBeanToCsvBuilder<Voyage>(writer)
+                    .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .withSeparator(';')
+                    .withOrderedResults(false)
+                    .build();
+            writers.write(list);
+            writer.close();
+            Resource resource = new ClassPathResource("/" + filename);
+            String contentType = null;
+            try {
+                contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+            } catch (IOException ex) {
+                log.info("Could not determine file type.");
+            }
+            // Fallback to the default content type if type could not be determined
+            if (contentType == null) {
+                contentType = "application/octet-stream";
+            }
+
+            System.out.println(resource.getFilename());
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(contentType))
+                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                    .body(resource);
+        } else return ResponseEntity.notFound().build();
+
     }
+
 
     @GetMapping("/all")
     public List<Voyage> getVoyages() {
